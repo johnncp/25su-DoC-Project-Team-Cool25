2afc7680b704933a99037c4f8008dae4749f2fb3
johnncp <johnnguyencaophuc@gmail.com>
2025-05-27 19:15:48 +0200

    Added insertions of fabricated data for tests into SQL DDL

--- database-files/eurobebe.sql
+++ database-files/eurobebe.sql
@@ -133,3 +133,61 @@ CREATE TABLE ChildcareOptions
     FOREIGN KEY (user_id) REFERENCES User (user_id)
         ON UPDATE cascade ON DELETE restrict
 );
+
+
+-- # INSERTING TEMPORARY "DATA":
+-- ## Users (politicians, daycare operators, to-be parents)
+INSERT INTO User (user_id, name, age, occupation, country) VALUES
+(1, 'Mark Fontenot', 80, 'Politician', 'France'),
+(2, 'Eric Gerber', 80, 'Daycare Owner', 'Germany');
+
+-- ## YEAR
+INSERT INTO Year (year, user_id) VALUES
+(2023, 1),
+(2024, 2);
+
+-- ## EUBirthData
+INSERT INTO EUBirthData (eubd_id, country_code, birth_rate, crude_birth_rate, year) VALUES
+(1, 'BE', 10.8, 10.9, 2023),
+(2, 'DE', 9.3, 9.4, 2024);
+
+-- ## EUEmployment
+INSERT INTO EUEmployment (eue_id, year, country_code, workforce, self_employment, work_hours_weekly, sex, industry_sector) VALUES
+(1, 2023, 'BE', 29500000, 11.2, 36.5, 'Female', 'Transportation'),
+(2, 2024, 'DE', 44800000, 9.8, 35.2, 'Male', 'Finance');
+
+-- ## Children_FamilyBenefits
+INSERT INTO Children_FamilyBenefits (cfb_id, year, country_code, support_program, dependent_type, euro_amount) VALUES
+(1, 2023, 'BE', '$ for families', 'Child under 18', 1912.12),
+(2, 2024, 'DE', 'Alien Assistance', 'First and Second Child of Asylum Seekers', 25.00);
+
+-- ## PolicyAnalysis
+INSERT INTO PolicyAnalysis (analysis_id, user_id, country_code) VALUES
+(1, 1, 'BE'),
+(2, 2, 'DE');
+
+-- ## PolicyDetails
+INSERT INTO PolicyDetails (details_id, cost_per_month, policy_type, effect_on_birth_rate, analysis_id) VALUES
+(1, 450.00, 'Universal Childcare', 2.3, 1),
+(2, 320.00, 'Parental Leave', 1.8, 2);
+
+-- ## BusinessPlanning
+INSERT INTO BusinessPlanning (plan_id, daycare_id, year_forecast, user_id) VALUES
+(1, 101, 2026, 2),
+(2, 102, 2028, 2);
+
+-- ## GeneralLogistics
+INSERT INTO GeneralLogistics (oper_id, staffing_demand, financial_analysis, plan_id) VALUES
+(1, 12, 'Projected: €580,000/year. Staff: €380,000. Operating: 1.5%', 1),
+(2, 18, 'Expansion needs €250,000 investment.ROI: 22%', 2);
+
+-- ## OperatingHours
+INSERT INTO OperatingHours (hours_id, plan_id, year_forecast, daycare_id) VALUES
+(1, 1, 2025, 101),
+(2, 2, 2026, 102);
+
+-- ## ChildcareOptions
+INSERT INTO ChildcareOptions (option_id, country_code, cost_per_month, user_id) VALUES
+(1, 'BE', 650.00, 1),
+(2, 'DE', 450.00, 2);
+
