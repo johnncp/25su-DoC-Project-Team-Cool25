6b102ccdd943ac11350a48c27eb47b2238587d18
sophiefarrell <farrell.so@northeastern.edu>
2025-06-11 22:09:18 +0200

    fixed business planner page so all buttons work

--- app/src/pages/04_Business_Planner.py
+++ app/src/pages/04_Business_Planner.py
@@ -1,4 +1,5 @@
 import logging
+
 logger = logging.getLogger(__name__)
 
 import streamlit as st
@@ -7,7 +8,15 @@ import requests
 from streamlit_extras.app_logo import add_logo
 import datetime
 
-st.set_page_config(layout = 'wide')
+if st.session_state.get("go_to_daycare_profile"):
+    logger.info("in the if true stmt")
+    st.session_state["go_to_daycare_profile"] = False
+    st.switch_page("pages/06_Daycare_Profile.py")
+else:
+    logger.info("in the else")
+    logger.info(st.session_state)
+
+st.set_page_config(layout="wide")
 
 ## need to change name soon
 
@@ -15,56 +24,61 @@ st.set_page_config(layout = 'wide')
 SideBarLinks()
 
 st.title(f"Welcome Daycare Owner, {st.session_state['first_name']}.")
-st.write('')
-st.write('')
-st.write('### What would you like to do today?')
+st.write("")
+st.write("#### What would you like to do today?")
+st.write("")
 
-viewLocations = False
+viewLocations = st.session_state.get('view_locations', False)
 addLocation = False
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns([2,1])
 
-with col3:
-    if st.button('Add a new location', 
-             type='primary',
-             use_container_width=True):
-      API_URL = "http://web-api:4000/location//locations"
-      st.switch_page('pages/05_Add_Location.py')
+
 
 with col2:
-  if st.button('Research new locations', 
-                type='primary',
-              use_container_width=True):
-      st.write("Feature coming soon!")
+    st.write("\n\n")
+    st.write("\n\n")
+    st.write("\n\n")
+    st.write("\n\n")
+    st.write("\n\n")
+    st.write("\n\n")
+    st.write("\n\n")
+    if st.button("Research new locations", type="primary", use_container_width=True):
+        st.write("Feature coming soon!")
+    if st.button("Add a new location", type="primary", use_container_width=True):
+        API_URL = "http://web-api:4000/location/locations"
+        st.switch_page("pages/05_Add_Location.py")
 
-with col1: 
-  if st.button('See current locations',
-              type='primary',
-              use_container_width=True):
-    API_URL = "http://web-api:4000/location/locations"
-    #might not work
-    viewLocations = True
-    try: 
-      response = requests.get(API_URL)
-      if response.status_code == 200:
-        locations = response.json()
+with col1:
+    #if st.button("See current locations", type="primary", use_container_width=True):
+        #st.session_state['view_locations'] = True
+        #st.rerun()
+        st.write("### View Current Locations Below: ")
 
-      st.write(f"Found {len(locations)} Locations")
-      logger.info("Before the for loop")
-      for loc in locations:
-         logger.info(f"location = {loc}")
-         with st.container(border = True):
-            col1, col2 = st.columns(2)
-            with col1:
-                st.write(loc['daycare_name'])
-            with col2: 
-               if st.button('View Full Profile', key=loc['daycare_id']):
-                  st.session_state['selected_daycare_id'] = loc['daycare_id']
-                  logger.info(f"session state = {st.session_state}")
-                  st.switch_page('pages/06_Daycare_Profile.py')
+        if viewLocations:
+            API_URL = "http://web-api:4000/location/locations"
+            try:
+                response = requests.get(API_URL)
+                if response.status_code == 200:
+                    locations = response.json()
 
+                st.write(f"Found {len(locations)} Locations")
+                logger.info("Before the for loop")
+                for loc in locations:
+                    logger.info(f"location = {loc}")
+                    with st.container(border=True):
+                        col1, col2 = st.columns(2)
+                        with col1:
+                            st.write(loc["daycare_name"])
+                        with col2:
+                            if st.button(
+                                f'View Full Profile',
+                                key=loc["daycare_id"],
+                            ):
+                                logger.info("in the button execution...")
+                                st.session_state["selected_daycare_id"] = loc["daycare_id"]
+                                st.session_state["go_to_daycare_profile"] = True
+                                st.rerun()
 
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error connecting to the API: {str(e)}")
-        st.info("Please ensure the API server is running on http://web-api:4000")
-
-
+            except requests.exceptions.RequestException as e:
+                st.error(f"Error connecting to the API: {str(e)}")
+                st.info("Please ensure the API server is running on http://web-api:4000")
