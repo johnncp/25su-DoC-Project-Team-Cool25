7d8639ebfd4d5a4aeed0367f32a6fdb09e0a075a
johnncp <johnnguyencaophuc@gmail.com>
2025-06-13 01:16:17 +0200

    Fixed extra imports messing up API

--- api/backend/notes/notes_routes.py
+++ api/backend/notes/notes_routes.py
@@ -10,7 +10,6 @@ from flask import (
 import json
 from datetime import datetime
 from backend.db_connection import db
-from backend.simple.playlist import sample_playlist_data
 from backend.ml_models import model01
 
 # This blueprint handles some basic routes that you can use for testing
