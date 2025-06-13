e54ca30339b704df90a6e47d0afe38b3d97a0b32
sophiefarrell <farrell.so@northeastern.edu>
2025-06-13 04:07:29 +0200

    got rid of commented out code

--- app/src/pages/07_Daycare_Research.py
+++ app/src/pages/07_Daycare_Research.py
@@ -26,8 +26,6 @@ st.markdown("""
 - Is this market oversaturated or still growing?
 """)
 
-
-
 # 1. Get all locations (only active ones returned by your route)
 locations_response = requests.get("http://web-api:4000/location/locations")
 locations_data = locations_response.json()
@@ -100,84 +98,6 @@ fig = px.bar(
 fig.update_layout(xaxis_title="Year", yaxis_title="Average Enrollment")
 fig.show()
 
-
-
-
-
-
-# # datasets
-# df_data = pd.read_csv("datasets/parent/DaycareData.csv")
-# df_locations = pd.read_csv("datasets/parent/DaycareLocations.csv", encoding="ISO-8859-1")
-
-
-# # Merge and filter
-# df = pd.merge(df_data, df_locations, on="daycare_id")
-# df = df[df["inactive"] == False]  # active only
-# df["year"] = df["year"].astype(int)
-# df["daycare_display_name"] = df["daycare_name"] + " (" + df["city"] + ")"
-
-# # create country map
-# country_map = {
-#     'BE': 'Belgium',
-#     'BG': 'Bulgaria',
-#     'CZ': 'Czechia',
-#     'DK': 'Denmark',
-#     'DE': 'Germany',
-#     'EE': 'Estonia',
-#     'IE': 'Ireland',
-#     'EL': 'Greece',
-#     'ES': 'Spain',
-#     'FR': 'France',
-#     'HR': 'Croatia',
-#     'IT': 'Italy',
-#     'CY': 'Cyprus',
-#     'LV': 'Latvia',
-#     'LT': 'Lithuania',
-#     'LU': 'Luxembourg',
-#     'HU': 'Hungary',
-#     'MT': 'Malta',
-#     'NL': 'Netherlands',
-#     'AT': 'Austria',
-#     'PL': 'Poland',
-#     'PT': 'Portugal',
-#     'RO': 'Romania',
-#     'SI': 'Slovenia',
-#     'SK': 'Slovakia',
-#     'FI': 'Finland',
-#     'SE': 'Sweden'
-# }
-
-# # Get sorted country list
-# # available_countries = sorted(df["country_code"].unique())
-# # selected_country = st.selectbox("Select a country to research:", available_countries)
-
-# # Get full country names
-# available_countries = sorted([country_map[code] for code in df["country_code"].unique() if code in country_map])
-# selected_country_name = st.selectbox("Select a country to research:", available_countries)
-
-# # Reverse lookup to get the country code
-# reverse_map = {v: k for k, v in country_map.items()}
-# selected_country = reverse_map[selected_country_name]
-
-# # --- LINE CHART: Avg enrollment over time ---
-# trend_df = df[df["country_code"] == selected_country].groupby("year")["enrollment"].mean().reset_index()
-
-# fig_line = go.Figure()
-# fig_line.add_trace(go.Scatter(
-#     x=trend_df["year"],
-#     y=trend_df["enrollment"],
-#     mode='lines+markers',
-#     name=selected_country
-# ))
-
-# fig_line.update_layout(
-#     title=f"Avg Enrollment Over Time – {selected_country_name}",
-#     xaxis_title="Year",
-#     yaxis_title="Average Enrollment",
-#     height=500
-# )
-
-# #st.plotly_chart(fig_line, use_container_width=True)
 col1, col2 = st.columns([2, 1])
 
 with col1:
@@ -187,43 +107,7 @@ with col2:
         st.write("\n\n")
     st.markdown(f"See how daycare enrollment has changed in **{selected_country}** year by year. This can help you spot growth trends and estimate demand.")
 
-# # --- BAR CHART: Enrollment + Staff by daycare for latest year ---
-# latest_year = df["year"].max()
-# bar_df = df[(df["year"] == latest_year) & (df["country_code"] == selected_country)].copy()
-# bar_df.sort_values(by="enrollment", ascending=False, inplace=True)
-
-# fig_bar = go.Figure()
-# fig_bar.add_trace(go.Bar(
-#     x=bar_df["daycare_display_name"],
-#     y=bar_df["enrollment"],
-#     name="Enrollment",
-#     marker_color="steelblue"
-# ))
-# fig_bar.add_trace(go.Bar(
-#     x=bar_df["daycare_display_name"],
-#     y=bar_df["staff"],
-#     name="Staff",
-#     marker_color="indianred"
-# ))
-
-# fig_bar.update_layout(
-#     title=f"Enrollment vs. Staff in {selected_country_name} ({latest_year})",
-#     barmode="group",
-#     xaxis=dict(
-#         title="Daycare (with city)",
-#         tickangle=45,
-#         tickfont=dict(size=11),
-#         title_font=dict(size=14),
-#     ),
-#     yaxis=dict(
-#         title="Count",
-#         title_font=dict(size=14),
-#         tickformat=",",
-#         tickfont=dict(size=12),
-#     ),
-#     height=600
-# )
-
+# --- BAR CHART: Enrollment + Staff by daycare for latest year ---
 
 # --- Filter to latest year ---
 latest_year = df["year"].max()
